3013a307fb5c32fbaad8a490b9ab7ad5d7ddb49d
Bartosz Sadecki <bartosz.sadecki+git@posteo.pl>
2025-04-10 17:26:01 +0200

    modify ansible-vault aliases

--- zshrc/ansible.sh
+++ zshrc/ansible.sh
@@ -1,5 +1,9 @@
 # string encryption and decryption using vault
-alias anen='ansible-vault encrypt_string'
-function ande {
+alias aven='ansible-vault encrypt_string'
+function avde {
   echo $@ | tr -d ' ' | ansible-vault decrypt --output -
 }
+
+# viewing and editing vault encrypted files
+alias avv='ansible-vault view'
+alias ave='ansible-vault edit'
