7fb1c3884f06cc1691a3677f0475a950d3b1e1ce
Bartosz Sadecki <bartosz.sadecki@smartbear.com>
2025-09-23 12:48:46 +0200

    improve the find workaround for mac

--- zshrc/mac.sh
+++ zshrc/mac.sh
@@ -1,8 +1,8 @@
 [ "$OS" = 'mac' ] || return
 
 function find {
-  if [ "$#" -eq 0 ]; then
-    command find .
+  if [[ "$#" -eq 0 || "$1" == -* ]]; then
+    command find . "$@"
   else
     command find "$@"
   fi
